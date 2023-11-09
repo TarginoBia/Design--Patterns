6f63644e238cd26d0f9822b5fefea796acd74557
Maria Beatriz Targino <112200332+TarginoBia@users.noreply.github.com>
2023-11-09 16:05:58 -0300

    Add files via upload

---
+++ interpreter/Numero.java
@@ -0,0 +1,16 @@
+package interpreter;
+
+public class Numero implements Operador {
+
+	private int numero;
+
+	public Numero(int numero) {
+		this.numero = numero;
+	}
+
+	@Override
+	public int interpretar() {
+		return this.numero;
+	}
+}
+
